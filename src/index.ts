54136fd62ea3267c2104958f49b34f8a1b93bf27
Jackson Taylor <jackson@jksntaylor.com>
2021-09-24 15:00:03 -0600

    useRoundRectGlow

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { useCounter } from './hooks/useCounter/';
 export { useEllipseGlow } from './hooks/useEllipseGlow'
 export { useAnimationFrame } from './hooks/useAnimationFrame'
+export { useRoundRectGlow } from './hooks/useRoundRectGlow'
